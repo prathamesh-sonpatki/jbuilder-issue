a751c331d47c0da7a151f3042a8b603475171d4b
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-06-07 14:44:44 -0700

    Ran generator `after` installing jbuilder, see the diff in controller
    - The controller is now rendering views using `render :show` and so on
      instead of `render json: @post`.
    - Because of this change we need the `as: :json` helper in the tests to
      indicate that the request is JSON. Otherwise the request is considered
      as HTML.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,38 +2,40 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
   # GET /posts
+  # GET /posts.json
   def index
     @posts = Post.all
-
-    render json: @posts
   end
 
   # GET /posts/1
+  # GET /posts/1.json
   def show
-    render json: @post
   end
 
   # POST /posts
+  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
     if @post.save
-      render json: @post, status: :created, location: @post
+      render :show, status: :created, location: @post
     else
       render json: @post.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
   def update
     if @post.update(post_params)
-      render json: @post
+      render :show, status: :ok, location: @post
     else
       render json: @post.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /posts/1
+  # DELETE /posts/1.json
   def destroy
     @post.destroy
   end
@@ -44,7 +46,7 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title)
     end
